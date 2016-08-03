d103ceff6061eb169af34fda9ce725d1a6389f50
Andrew Lee <andrewlee1905@gmail.com>
2016-08-04 01:43:04 +0800

    no message

--- supper4ubot.js
+++ supper4ubot.js
@@ -1,7 +1,7 @@
 var unirest = require('unirest');
 var BASE_URL = 'https://api.telegram.org/bot215283677:AAHCsBfr9quGh9W16gDAmlXJzCykvhmMJGA'
 var POLLING_URL = BASE_URL + "getUpdates?offset=:offset:&timeout=60";
-var SEND_MESSAGE_URL = BASE_URL + "sendMessage";
+var SEND_MESSAGE_URL = BASE_URL + "sendMessage";</pre>
 
 function poll(offset) {
   var URL = pollingURL.replace(":offset:", offset);
@@ -46,7 +46,14 @@ function runCommand(message) {
   var msgtext = message.text;
   if (msgtext.indexOf("/") != 0) return false;
   var command = msgtext.substring(1, msgtext.indexOf(" "));
-  if (COMMANDS[command] == null) return false;
+  if (COMMANDS[command] == null) {
+    var answer = {
+      chat_id : message.chat.id,
+      text : "not a command "
+    };
+    .send(answer)
+    return false
+  };
 
   COMMANDS[command](message);
   return true;
